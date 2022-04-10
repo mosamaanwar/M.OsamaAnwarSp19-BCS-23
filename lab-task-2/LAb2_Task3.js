1c4eed464fa30930bf415074fd1c4ad979abb907
mosamaanwar <100298225+mosamaanwar@users.noreply.github.com>
2022-04-10 16:04:07 +0500

    Create LAb2_Task3.js

---
+++ lab-task-2/LAb2_Task3.js
@@ -0,0 +1,26 @@
+let library = [ 
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }];
+ for (let i = 0; i < library.length; i++) 
+   {
+    let book = "'" + library[i].title + "'" + ' by ' + library[i].author +' ';
+    if (library[i].readingStatus) {
+      console.log(  book+ "true ");
+    } else
+    {
+     console.log(  book + "false ");
+    }
+   };
